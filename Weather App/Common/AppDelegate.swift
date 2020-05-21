94ab34e8bcf77ca1f4a8736390a96554345dc27d
krokonox <kuricyna20@gmail.com>
2020-05-21 14:28:51 +0300

    first commit

---
+++ Weather App/Common/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Weather App
+//
+//  Created by Admin on 21/05/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
